c683ca5c401fa18f752d87ddbb348d8cdafd643d
Schrese <schreseholloway@gmail.com>
2020-07-10 14:13:36 -0400

    finished my 3rd problem

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    counts = {}
+    result = []
+    for elem in a:
+        # print(abs(elem))
+        minimized = abs(elem)
+        if minimized not in counts:
+            counts[minimized] = 0
+        counts[minimized] += 1
+    print(counts)
+    for elem in counts:
+        if counts[elem] >= 2:
+            result.append(elem)
     return result
 
 
