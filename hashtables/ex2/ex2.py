ae1401a60d6f296b742831c9eb163d47c2e97e95
Schrese <schreseholloway@gmail.com>
2020-07-10 12:57:18 -0400

    done with first one will add in written notes later

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,24 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = [None] * length
 
+    ticket_jumble = {}
+    # start = ''
+    for t in tickets:
+        # print(t.source, t.destination)
+        ticket_jumble[t.source] = t.destination
+
+    pointer = ticket_jumble['NONE']
+    for index in range(0, length):
+        route[index] = pointer
+        pointer = ticket_jumble[pointer]
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
